1163c9f92ca0222aec40ccd3f68c49778d353c82
Arindom Paul <arindom.paul123@gmail.com>
2026-05-11 17:52:36 -0400

    Fixing logical errors, still need to finalize train.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import chess
+import torch
+import os
 from model import ChessNet
 from engine import findBestMove
 from train import trainEngine
+numGames = 100
 listBoardStates = []
-model = ChessNet();
-board = chess.Board();
-while not board.is_game_over():
-    board.push(findBestMove(board, model))
-    listBoardStates.append[board]
-trainEngine(listBoardStates, 1, model)
+model = ChessNet()
+if os.path.exists("internalWeights.pth"):
+    model.load_state_dict(torch.load("internalWeights.pth", weights_only = True))
+optimizer = torch.optim.Adam(model.parameters(),lr=.001)
+for _ in range(numGames):
+    board = chess.Board()
+    while not board.is_game_over():
+        listBoardStates.append(board.copy())
+        board.push(findBestMove(board, model))
+    listBoardStates.append(board.copy())
+    trainEngine(listBoardStates, model, optimizer)
+    listBoardStates = []
+torch.save(model.state_dict(), "internalWeights.pth")
--- train.py
+++ train.py
@@ -1,19 +1,19 @@
-import chess
 import torch
-from main import model
 from engine import findBestMove
 
-optimizer = torch.optim.Adam(model.parameters(),lr=.001)
-
-def trainEngine(listBoardStates, numTimes, model):
-    for _ in range (numTimes):
-        optimizer.zero_grad()
-        groundTruth = listBoardStates[-1].result();
-        for board in listBoardStates:
-            numEvals = 0
-            numEvals = numEvals + 1
-            predictedEval = model(board)
-            squaredErrorSums += (predictedEval - groundTruth)**2
-        meanSquaredError = squaredErrorSums/numEvals
-        meanSquaredError.backward()
-        optimizer.step()
+def trainEngine(listBoardStates, model, optimizer):
+    optimizer.zero_grad()
+    results = listBoardStates[-1].result()
+    groundTruth = 0
+    if len(results) < 6:
+        groundTruth = -1 if results[1] == 0 else 1
+    else: groundTruth = 0
+    numEvals = 0
+    squaredErrorSums = 0
+    for board in listBoardStates:
+        numEvals = numEvals + 1
+        predictedEval = model(board)
+        squaredErrorSums += (predictedEval - groundTruth)**2
+    meanSquaredError = squaredErrorSums/numEvals
+    meanSquaredError.backward()
+    optimizer.step()
